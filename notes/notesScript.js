fa4ff8a29d25235d081eabaef46a2726c890a0fd
theauspiciouscammy <theauspiciouscammy@gmail.com>
2022-06-06 19:40:27 -0700

    update

---
+++ notes/notesScript.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+    // console.log( "ready!" );
+    function custom() {
+        // we can include more code here if we want to
+        console.log("this message is coming from script.js");
+    }
+    // custom();
+    
+});
+
